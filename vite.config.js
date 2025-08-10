0eaaeb05c02fa902e3ce584cdfe7b25b4e6d0a23
Yodahe Teshome <jodahe2001@gmail.com>
2025-08-10 22:10:59 +0300

    Configure port for Render

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
+  plugins: [react()],
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  server: {
+    port: 4173,
+    strictPort: true,
+  }
 })
