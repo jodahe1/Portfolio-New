d7774637264178810382b59bfb80140ab03ecd85
Yodahe Teshome <jodahe2001@gmail.com>
2025-08-10 21:44:56 +0300

    new project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,8 +12,16 @@ const Projects = () => {
         imgSrc='/assets/educonnect.png'
           title = "edu-connect" 
           description = "The goal of the project is to build a centralized system which can connect stakeholders in school like parent , teacher and students beyond that it also have admin dashboard which allow the school board to manage different aspects. and it have payment processing which is done with stripe api. you can check more about it using the github link to the project."/>
-<Card 
-        webLink="https://fikrudemlew.vercel.app/"
+
+         <Card
+        webLink="https://github.com/jodahe1/propertymanagment.git"
+        imgSrc='/assets/propertymgt.png'
+          title = "Property Management" 
+          description = "On this project i build a property management system using nest js it can be used by hotels , apartment owners it help them to manage their property easily and it allow users(customers) in the case of hotels to make a booking , manage their bookings. It's all back end for now i didn't build the front end for it you can check more about by clicking the button below. "/>
+
+      
+          <Card 
+        webLink="https://github.com/jodahe1/Portfolio-New.git"
         imgSrc='/assets/Portfolioimg.jpg'
         title="Portfolio"
         description="Create a clean, professional, and responsive portfolio website to showcase my personal projects, skills, resume, and contact information. The site should include a homepage with a short introduction, a projects section with images and descriptions, an about page with my background and skills, and a contact form. Use modern design elements, smooth scrolling, and subtle animations to enhance user experience"/>
