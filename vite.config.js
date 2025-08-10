13df3822239913ecb8da7ed196dee7ee1e3e1067
Yodahe Teshome <jodahe2001@gmail.com>
2025-08-10 22:25:02 +0300

    vite rollback

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 4173,
-    strictPort: true,
-    host: true, // Add this line
-    allowedHosts: [
-      'portfolio-new-8l25.onrender.com', //  Render URL
-      'localhost' // For local development
-    ]
-  },
-  server: {
-    port: 4173,
-    strictPort: true,
-    host: true
-  }
+  plugins: [
+    tailwindcss(),
+    react()],
 })
