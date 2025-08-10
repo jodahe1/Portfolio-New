843602a0ddec6211e837b31557c1783124a26e72
Yodahe Teshome <jodahe2001@gmail.com>
2025-08-10 22:16:33 +0300

    Add allowed hosts to Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,15 @@ export default defineConfig({
   preview: {
     port: 4173,
     strictPort: true,
+    host: true, // Add this line
+    allowedHosts: [
+      'portfolio-new-8l25.onrender.com', //  Render URL
+      'localhost' // For local development
+    ]
   },
   server: {
     port: 4173,
     strictPort: true,
+    host: true
   }
 })
